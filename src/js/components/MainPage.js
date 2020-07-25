9be381b480dfd46486349c24b5e3218192456101
StaRen <starendev@gmail.com>
2020-07-26 01:52:21 +0300

    did login page, actions, user reducer, created state

--- src/js/components/MainPage.js
+++ src/js/components/MainPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 
 export const MainPage = () => {
@@ -9,7 +9,7 @@ export const MainPage = () => {
                 <p className="main-page__welcome-block__text">
                     Welcome to the <a href={"https://github.com/StaRenn"}>StaRen's</a> news searching service
                 </p>
-                <Link className={"main-page__welcome-block__get-started"} to={"/news"}>Get Started</Link>
+                <Link className={"main-page__welcome-block__get-started"} to={localStorage.getItem("authorized") ? "/news": "login"}>Get Started</Link>
             </div>
         </div>
     )
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Root} from './components/Root'
+import {Root} from './Root'
 import "../styles/main.css"
 
 render(<Root />, document.querySelector('body'));
